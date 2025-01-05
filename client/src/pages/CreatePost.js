b760f882c199bd4c883507fd393800ee51e3cded
Kyle Deng <kyle.deng01@gmail.com>
2025-01-04 21:16:09 -0500

    ID pages

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -2,9 +2,10 @@ import React from 'react';
 import {Formik, Form, Field, ErrorMessage} from 'formik';
 import * as Yup from 'yup';
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function CreatePost() {
-
+    let navigate = useNavigate();
     const initialValues = {
         title: "",
         postText: "",
@@ -19,7 +20,7 @@ function CreatePost() {
 
     const onSubmit = (data) => {
         axios.post("http://localhost:3001/posts", data).then((response) => {
-            console.log("IT WORKED");
+            navigate("/");
         });    
     };
 
